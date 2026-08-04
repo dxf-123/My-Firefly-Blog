61e50f16cd2060926b85a01fac93b63e1b74908e
StaRRy <3378519450@qq.com>
2026-08-05 01:51:41 +0800

    feat:调整qq页

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "StaRRy",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm StaRRy.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,13 +24,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/q/E26a7phJni",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/dxf-123",
 			showName: false,
 		},
 		{
