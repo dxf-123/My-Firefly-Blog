658408334a8e5446ec96e260b745a60dbd66453d
StaRRy <3378519450@qq.com>
2026-08-05 18:18:45 +0800

    feat:背景页

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -39,22 +39,25 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/e1.jpg",
+			"assets/images/DesktopWallpaper/e2.jpg",
+			"assets/images/DesktopWallpaper/e3.jpg",
+			"assets/images/DesktopWallpaper/e4.jpg",
+			"assets/images/DesktopWallpaper/e5.jpg",
+			"assets/images/DesktopWallpaper/e6.jpg",
+			"assets/images/DesktopWallpaper/e7.jpg",
+			"assets/images/DesktopWallpaper/e8.jpg",
+			"assets/images/DesktopWallpaper/e9.jpg",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/MobileWallpaper/w1.jpg",
+			"assets/images/MobileWallpaper/w2.jpg",
+			"assets/images/MobileWallpaper/w3.jpg",
+			"assets/images/MobileWallpaper/w4.jpg",
+			"assets/images/MobileWallpaper/w5.jpg",
+			"assets/images/MobileWallpaper/w6.jpg",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
@@ -73,7 +76,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Star! StaRRy!",
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
